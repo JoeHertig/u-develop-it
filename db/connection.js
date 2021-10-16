4e6c65f70c74cf2f2a42ebc1b6749b382b651720
JoeHertig <87396188+JoeHertig@users.noreply.github.com>
2021-10-16 14:33:49 -0600

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
   // Your MySQL username,
   user: "root",
   // Your MySQL password
-  password: "9dog-tree-ball4",
+  password: "",
   database: "election",
 });
 
